0812e4093e15fa1b7f11edd8dfa06e82a0b8b6e0
Ivan Pejic <pejicbgd@hotmail.com>
2018-01-07 22:56:47 +0100

    Structure

--- wp-content/themes/hejwess/footer.php
+++ wp-content/themes/hejwess/footer.php
@@ -11,10 +11,72 @@
 
 	</div><!-- .site-content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			
-		</div><!-- .site-info -->
+	<footer class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-3">
+				<img src="" alt="" />
+			</div>
+
+			<div class="col-xs-12 col-md-3">
+				<h4>Quick links</h4>
+				<ul>
+					<li>
+						<a href="#" title="">Link 1</a>
+					</li>
+					<li>
+						<a href="#" title="">Link 2</a>
+					</li>
+					<li>
+						<a href="#" title="">Link 3</a>
+					</li>
+					<li>
+						<a href="#" title="">Link 4</a>
+					</li>
+					<li>
+						<a href="#" title="">Link 5</a>
+					</li>
+					<li>
+						<a href="#" title="">Link 6</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="col-xs-12 col-md-3">
+				<h4>Strategic leadership</h4>
+				<ul>
+					<li>
+						<a href="#" title="">About me</a>
+					</li>
+					<li>
+						<a href="#" title="">Services</a>
+					</li>
+					<li>
+						<a href="#" title="">Events</a>
+					</li>
+					<li>
+						<a href="#" title="">Blog</a>
+					</li>
+					<li>
+						<a href="#" title="">Contact</a>
+					</li>
+					<li>
+						<a href="#" title="">Login</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="col-xs-12 col-md-3">
+				<h4>Contact</h4>
+				<p>telefone 1<p>
+				<p>telefone 2<p>
+				<h4>Sign up for a newsletter</h4>
+				<form method="post" action="">
+					<input type="email" name="email" required />
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+
+		</div>
 	</footer><!-- .site-footer -->
 
 </div><!-- .site -->
--- wp-content/themes/hejwess/index.php
+++ wp-content/themes/hejwess/index.php
@@ -15,9 +15,9 @@ get_header(); ?>
 			<div class="container">
 				<div class="row">
 
-					<div class="second">
+					<div class="two">
 						<div class="left col-xs-12 col-md-6">
-							what is this all about?<br /><span>vision<span>
+							<h2>what is this all about?<br /><span>vision<span></h2>
 						</div>
 
 						<div class="right col-xs-12 col-md-6">
@@ -28,7 +28,7 @@ get_header(); ?>
 				</div>
 
 				<div class="row">
-					<div class="third">
+					<div class="three">
 						<div class="left col-xs-12 col-md-6">
 							Innovacion organizational
 						</div>
@@ -44,9 +44,9 @@ get_header(); ?>
 				</div>
 
 				<div class="row">
-					<div class="forth">
+					<div class="four">
 						<div class="left col-xs-12 col-md-6">
-							what we do?
+							<h2>what we do?</h2>
 						</div>
 
 						<div class="right col-xs-12 col-md-6">
@@ -57,7 +57,7 @@ get_header(); ?>
 				</div>
 
 				<div class="row">
-					<div class="fifth">
+					<div class="five">
 						<div class="section col-xs-12 col-md-6">
 							<img src="" alt="" />
 							<h3>espendioensoodo asdj kas</h3>
@@ -86,9 +86,9 @@ get_header(); ?>
 
 				<div class="row">
 
-					<div class="sixth">
+					<div class="six">
 						<div class="col-xs-12 col-md-6">
-							who am I?<br /><span>Idealista<span>
+							<h2>who am I?<br /><span>Idealista<span></h2>
 							<br />
 							<br />
 							simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
@@ -104,6 +104,262 @@ get_header(); ?>
 						</div>
 					</div>
 				</div>
+				<div class="row">
+					<div class="seven">
+						<div class="col-xs-12 col-md-6">
+							simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+							dummy text ever since the 1500s, when an unknown
+						</div>
+
+						<div class="col-xs-12 col-md-6">
+							<h2>who am I?<br /><span>Idealista<span></h2>
+							<br />
+							<br />
+							simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+							dummy text ever since the 1500s, when an unknown
+						</div>
+					</div>
+				</div>
+
+				<div class="row eight">
+					<div class="line"></div>
+					<div class="col-xs-12 col-md-4">
+						<span>1</span>
+						<p>metodologiast neki racq</p>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<span>2</span>
+						<p>metodologiast neki racq</p>
+					</div>
+					<div class="col-xs-12 col-md-4 clearfix">
+						<span>3</span>
+						<p>metodologiast neki racq</p>
+					</div>
+				</div>
+				<div class="row eight">
+					<div class="line"></div>
+					<div class="col-xs-12 col-md-4">
+						<span>4</span>
+						<p>metodologiast neki racq</p>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<span>5</span>
+						<p>metodologiast neki racq</p>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<span>6</span>
+						<p>metodologiast neki racq</p>
+					</div>
+				</div>
+				<div class="row eight">
+					<div class="line"></div>
+					<div class="col-xs-12 col-md-4">
+						<span>7</span>
+						<p>metodologiast neki racq</p>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<img src="" alt="" />
+						<p>find out more</p>
+					</div>
+				</div>
+
+				<div class="events-header">
+
+				</div>
+
+				<div class="row">
+					<div class="nine">
+						<div class="col-xs-12 col-md-6">
+							<h2>upcoming events<br /><span>workshops<span></h2>
+						</div>
+
+						<div class="col-xs-12 col-md-6">
+							simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+							dummy text ever since the 1500s, when an unknown
+						</div>
+					</div>
+				</div>
+
+				<div class="line"></div>
+
+				<div class="row">
+					<div class="ten">
+						<div class="col-xs-12 col-md-4">
+							<img src="" alt="" />
+							<h2>Discover you light</h2>
+							<p>January 31, 2018<br />at Barcelona</p>
+
+							<p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+							dummy text ever since the 1500s, when an unknown</p>
+
+							<span><a href="#" title="">Education</a></span>
+							<span><a href="#" title="">Performances</a></span>
+							<span><a href="#" title="">Light</a></span>
+
+							<div>
+								<img src="" alt="" />
+								<span>find out more</span>
+							</div>
+						</div>
+						<div class="col-xs-12 col-md-4">
+							<img src="" alt="" />
+							<h2>Discover you light</h2>
+							<p>January 31, 2018<br />at Barcelona</p>
+
+							<p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+							dummy text ever since the 1500s, when an unknown</p>
+
+							<span><a href="#" title="">Education</a></span>
+							<span><a href="#" title="">Performances</a></span>
+							<span><a href="#" title="">Light</a></span>
+
+							<div>
+								<img src="" alt="" />
+								<span>find out more</span>
+							</div>
+						</div>
+						<div class="col-xs-12 col-md-4">
+							<img src="" alt="" />
+							<h2>Discover you light</h2>
+							<p>January 31, 2018<br />at Barcelona</p>
+
+							<p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+							dummy text ever since the 1500s, when an unknown</p>
+
+							<span><a href="#" title="">Education</a></span>
+							<span><a href="#" title="">Performances</a></span>
+							<span><a href="#" title="">Light</a></span>
+
+							<div>
+								<img src="" alt="" />
+								<span>find out more</span>
+							</div>
+						</div>
+						<div class="line"></div>
+					</div>
+				</div>
+
+				<div class="blog-header">
+
+				</div>
+				<div class="row">
+					<div class="eleven">
+						<div class="col-xs-12 col-md-6">
+							<h2>let's exchange!<br /><span>Idealista<span></h2>
+						</div>
+
+						<div class="col-xs-12 col-md-6">
+							simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+							dummy text ever since the 1500s, when an unknown
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="twelve">
+						<div class="section col-xs-12 col-md-6">
+							<div class="col-xs-12">
+								<img src="" alt="" />
+							</div>
+							<div class="col-xs-12">
+								<h3>espendioensoodo asdj kas</h3>
+								<h6>dasdadadadada</h6>
+							</div>
+						</div>
+						<div class="section col-xs-12 col-md-6">
+							<div class="col-xs-12">
+								<img src="" alt="" />
+							</div>
+							<div class="col-xs-12">
+								<h3>espendioensoodo asdj kas</h3>
+								<h6>dasdadadadada</h6>
+							</div>
+						</div>
+						<div class="section col-xs-12 col-md-6">
+							<div class="col-xs-12">
+								<img src="" alt="" />
+							</div>
+							<div class="col-xs-12">
+								<h3>espendioensoodo asdj kas</h3>
+								<h6>dasdadadadada</h6>
+							</div>
+						</div>
+						<div class="section col-xs-12 col-md-6">
+							<h3>Submit your story</h3>
+							<div>
+								<img src="" alt="" />
+								I would like to submit my story
+							</div>
+						</div> 
+					</div>
+					
+				</div>
+
+				<div class="line"></div>
+
+				<div class="row">
+					<div class="thirteen">
+						<div class="section col-xs-12 col-md-6">
+							<h3>subscribe to my newsletter</h3>
+							<p>
+								simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+								dummy text ever since the 1500s, when an unknown
+							</p>
+							<span>me gustaria and some shit</span>
+						</div>
+
+						<div class="col-xs-12 col-md-6">
+							<img src="" alt="" />
+						</div>
+					</div>
+				</div>
+
+				<div class="talk-header">
+
+				</div>
+
+				<div class="row">
+					<div class="fourteen">
+						<div class="left col-xs-12 col-md-6">
+							<h2>hablamos?<br />also some othe shit text</h2>
+						</div>
+
+						<div class="right col-xs-12 col-md-6">
+							simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+							dummy text ever since the 1500s, when an unknown
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="three">
+						<div class="left col-xs-12 col-md-6">
+							Innovacion organizational
+						</div>
+
+						<div class="right col-xs-12 col-md-6">
+							Emprideiemnto exidos
+						</div>
+					</div>
+				</div>
+
+				<div class="talk-header">
+
+				</div>
+
+				<div class="row">
+					<div class="fifteen">
+						<div class="left col-xs-12 col-md-6">
+							<h2>testimonias de algunos clients</h2>
+						</div>
+
+						<div id="slider" class="right col-xs-12 col-md-6">
+							
+						</div>
+					</div>
+				</div>
+
+				<div class="line"></div>
 
 			</div>
 		</main><!-- .site-main -->

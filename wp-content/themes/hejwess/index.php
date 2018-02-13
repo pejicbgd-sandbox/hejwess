852a6ad22b57c82ae5bf59d510eb4ee888496f9e
Ivan <pejic.beograd@gmail.com>
2018-02-13 22:20:49 +0100

    Squash

--- wp-content/themes/hejwess/index.php
+++ wp-content/themes/hejwess/index.php
@@ -139,6 +139,15 @@ get_header(); ?>
 	</div>
 </div>
 
+<!--<div class="test">
+<div id="slide1" class="slide">
+  <div class="title">
+    <h1>Slide 1</h1>
+    <p>Lorem ipsum dolor sit amet, in velit iudico mandamus sit, persius dolorum in per, postulant mnesarchum cu nam. Malis movet ornatus id vim, feugait detracto est ea, eam eruditi conceptam in. Ne sit explicari interesset. Labores perpetua cum at. Id viris docendi denique vim.</p>
+  </div>
+</div>
+</div>-->
+
 <div class="clearfix main-wrap three">
 	<div class="left col-md-12 col-lg-6">
 		<span class="mb">
